c04dc25d03d277d80a18988f518982b9a856ca02
Aleksandr Korshunov <akor1191@mail.ru>
2023-12-23 19:41:26 +0300

    Added Zadacha 1

--- Zadachi/Program.cs
+++ Zadachi/Program.cs
@@ -1,1 +1,29 @@
-﻿
+﻿// Задача 1: Задайте значения M и N.
+// Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// Использовать рекурсию, не использовать циклы.
+
+// int num1 = new Random().Next(1,6);
+// System.Console.WriteLine("Наше первое число "+ num1);
+
+// int num2 = new Random().Next(6,13);
+// System.Console.WriteLine("Наше второе число "+ num2);
+
+// int firstElement = num1;
+
+// PrintNumbers(firstElement,num2);
+
+// void PrintNumbers(int startEl, int endEl)
+// {
+//     System.Console.WriteLine(startEl);
+
+//     if(startEl == endEl)
+//     {
+//         return;
+//     }
+
+// PrintNumbers(startEl+1, endEl);
+// }
+
+// Задача 2: Напишите программу вычисления функции Аккермана с помощью рекурсии.
+// Даны два неотрицательных числа m и n.
+
